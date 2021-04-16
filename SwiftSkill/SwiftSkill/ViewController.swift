3c501efe02660a1e2136bcf95bc33e0e2fcf4c24
王丹 <an.wang@zhan.onaliyun.com>
2021-04-16 15:40:07 +0800

    添加项目配置文件

--- SwiftSkill/SwiftSkill/ViewController.swift
+++ SwiftSkill/SwiftSkill/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        #if DEBUG
+            print("DEBUG 环境")
+        #endif
+
+        #if INTERNAL
+            print("INTERNAL 环境")
+        #endif
+
+        #if PRODUCTION
+            print("PRODUCTION 环境")
+        #endif
     }
 
 
